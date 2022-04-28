d5148657a99768193577a75544fa688668ff791b
Matthew Carlson <carlson.matt.r@gmail.com>
2022-04-27 21:09:03 -0700

    added most of get and post requests

--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,74 @@
 const express = require("express");
-const path = require('path')
+const path = require("path");
 const PORT = process.env.PORT || 3001;
+const util = require("util");
 
 const app = express();
 
+const fs = require("fs");
+const uuid = () =>
+  Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+
+const readFromFile = util.promisify(fs.readFile);
+/**
+ *  Function to write data to the JSON file given a destination and some content
+ *  @param {string} destination The file you want to write to.
+ *  @param {object} content The content you want to write to the file.
+ *  @returns {void} Nothing
+ */
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+/**
+ *  Function to read data from a given a file and append some content
+ *  @param {object} content The content you want to append to the file.
+ *  @param {string} file The path to the file you want to save to.
+ *  @returns {void} Nothing
+ */
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // app.use('/api', api);
 
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
+
+app.get("/api/notes", (req, res) => {
+  readFromFile("Develop/db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+app.post("/api/notes", (req, res) => {
+  const { noteTitle, noteText } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      noteTitle,
+      noteText,
+      note_id: uuid(),
+    };
+    readAndAppend(newNote, "Develop/db/db.json");
+    res.json(`Note added`);
+  } else {
+    res.error("Error in adding note");
+  }
+});
 
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
