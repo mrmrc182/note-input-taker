aa080e1b1b35c656e96e14933a9bb051296c889b
Matthew Carlson <carlson.matt.r@gmail.com>
2022-04-28 10:04:30 -0700

    morning progress

--- server.js
+++ server.js
@@ -12,22 +12,12 @@ const uuid = () =>
     .substring(1);
 
 const readFromFile = util.promisify(fs.readFile);
-/**
- *  Function to write data to the JSON file given a destination and some content
- *  @param {string} destination The file you want to write to.
- *  @param {object} content The content you want to write to the file.
- *  @returns {void} Nothing
- */
+
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
-/**
- *  Function to read data from a given a file and append some content
- *  @param {object} content The content you want to append to the file.
- *  @param {string} file The path to the file you want to save to.
- *  @returns {void} Nothing
- */
+
 const readAndAppend = (content, file) => {
   fs.readFile(file, "utf8", (err, data) => {
     if (err) {
@@ -44,14 +34,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// app.use('/api', api);
-
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
-);
-
 app.get("/api/notes", (req, res) => {
-  readFromFile("Develop/db/db.json").then((data) => res.json(JSON.parse(data)));
+  readFromFile("db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 app.post("/api/notes", (req, res) => {
@@ -63,12 +47,16 @@ app.post("/api/notes", (req, res) => {
       noteText,
       note_id: uuid(),
     };
-    readAndAppend(newNote, "Develop/db/db.json");
+    readAndAppend(newNote, "db/db.json");
     res.json(`Note added`);
   } else {
     res.error("Error in adding note");
   }
 });
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
